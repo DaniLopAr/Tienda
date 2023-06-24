02c8cead6bda6b627da5e88f898cc34b0385e7ae
Daniel Lopez <dj01lopeza@gmail.com>
2023-06-23 18:22:05 -0600

    al dia semana 5

---
+++ src/main/java/com/tienda/controller/CategoriaController.java
@@ -0,0 +1,26 @@
+
+package com.tienda.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import or.springframework.ui.Model;
+
+@Controller
+@RequestMapping("/categoria")
+public class CategoriaController {
+    
+    @Autowired
+    public CategoriaService categoriaService;
+    
+    
+    @GetMapping("/listado")
+    public String listado(Model model){
+        var categorias = categoriaService.getCategorias(false);
+        model.addAttribute("categorias", categorias);
+        model.addAttribute("totalcategorias", categorias.size());
+        return "/categoria/listado"
+    }
+    
+    
+}
